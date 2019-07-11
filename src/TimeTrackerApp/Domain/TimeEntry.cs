54dbcf9de2cbe4f4b0d508879e0f6ab319a0958e
Nina <nina@example.com>
2019-07-11 17:03:39 +0200

    Add domain models

---
+++ src/TimeTrackerApp/Domain/TimeEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackerApp.Domain
+{
+    public class TimeEntry
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public Project Project { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        public DateTime EntryDate { get; set; }
+
+        public int Hours { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public decimal HourRate { get; set; }
+    }
+}
