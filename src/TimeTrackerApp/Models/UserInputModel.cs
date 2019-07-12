2005f1344f1d9da9385628545377715bda0fc58d
Nina <nina@example.com>
2019-07-12 16:34:15 +0200

    Add view and input models

---
+++ src/TimeTrackerApp/Models/UserInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeTrackerApp.Domain;
+
+namespace TimeTrackerApp.Models
+{
+    public class UserInputModel
+    {
+        public string Name { get; set; }
+        public decimal HourRate { get; set; }
+
+        public void MapTo(User user)
+        {
+            user.Name = Name;
+            user.HourRate = HourRate;
+        }
+    }
+}
